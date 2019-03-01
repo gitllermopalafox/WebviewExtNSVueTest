2e81843edb7577bc1a3427bb9d4380dd708a4a27
Guillermo Palafox <palafox.87@gmail.com>
2019-03-01 02:18:20 -0600

    First commit

---
+++ app/components/App.vue
@@ -0,0 +1,40 @@
+<template>
+    <Page>
+        <ActionBar title="Welcome to NativeScript-Vue!"/>
+        <GridLayout columns="*" rows="*">
+
+            <WebViewExt
+                    row="0"
+                    src="~/assets/www/index.html"
+                    builtInZoomControls="false"
+                    displayZoomControls="false"
+                    supportZoom="false"
+
+            ></WebViewExt>
+
+        </GridLayout>
+    </Page>
+</template>
+
+<script >
+  export default {
+    data() {
+      return {
+      }
+    }
+  }
+</script>
+
+<style scoped>
+    ActionBar {
+        background-color: #53ba82;
+        color: #ffffff;
+    }
+
+    .message {
+        vertical-align: center;
+        text-align: center;
+        font-size: 20;
+        color: #333333;
+    }
+</style>
---
+++ app/main.js
@@ -0,0 +1,15 @@
+import Vue from 'nativescript-vue'
+import App from './components/App'
+import VueDevtools from 'nativescript-vue-devtools'
+import '@nota/nativescript-webview-ext/vue'
+
+if(TNS_ENV !== 'production') {
+  Vue.use(VueDevtools)
+}
+// Prints Vue logs when --env.production is *NOT* set while building
+Vue.config.silent = (TNS_ENV === 'production')
+
+
+new Vue({
+  render: h => h('frame', [h(App)])
+}).$start()
